53493f6fca433c6f01788ce00ffe4ef9c141c150
Bentleyjobs <bentleyjobs@gmail.com>
2020-04-23 16:13:59 +0800

    Ammend some enum util function

--- SophiaGL/test/enumTest.h
+++ SophiaGL/test/enumTest.h
@@ -1,79 +1,91 @@
-#pragma once
 #include<iostream>  
 #include <string>
 #include <sstream>
 #include <map>
 #include <vector>
+//private:																						            
+enum class MyEnum
+{
+	E_MEMBER1,
+	E_MEMBER2,
+	E_MEMBER3
+};
 
-#define __ENUM_TO_STRING(Type, ...)															                \
-class EnumReflectionHelper##Type																	        \
-{																								            \
-public:																							            \
-	EnumReflectionHelper##Type()																		    \
-	{																							            \
-		std::stringstream si(#__VA_ARGS__);														            \
-		std::string token;																		            \
-		while ((token = getName(si)).length())													            \
-		{																						            \
-			stringTable.push_back(#Type "::" + token);										                \
-			valueTable[#Type "::" + token] = valueTable[token] = stringTable.size() - 1;	                \
-		}																						            \
-	}																							            \
-	std::string toString(Type type) const													                \
-	{																							            \
-		return stringTable[static_cast<int>(type)];												            \
-	}																							            \
-	Type fromString(std::string s) const													                \
-	{																							            \
-		return static_cast<Type>(valueTable.at(s));	                                                        \
-	}																							            \
-	static EnumReflectionHelper##Type* getHelper()													        \
-	{																							            \
-		static EnumReflectionHelper##Type helper;														    \
-		return &helper;																			            \
-	}																							            \
-private:																						            \
-	static std::string getName(std::stringstream& si)											            \
-	{																							            \
-		std::string ret;																		            \
-		while (si &&																			            \
-			   !((si.peek() >= 'a' && si.peek() <= 'z') ||										            \
-			   (si.peek() >= 'A' && si.peek() <= 'Z') ||										            \
-			   (si.peek() >= '0' && si.peek() <= '9')))											            \
-			si.get();																			            \
-		if (!si) return "";																		            \
-		while ((si.peek() >= 'a' && si.peek() <= 'z') ||										            \
-			   (si.peek() >= 'A' && si.peek() <= 'Z') ||										            \
-			   (si.peek() >= '0' && si.peek() <= '9'))											            \
-			ret += si.get();																	            \
-		return ret;																				            \
-	}																							            \
-	std::vector<std::string> stringTable;														            \
-	std::map<std::string, int> valueTable;														            \
-};			
+//#define __S3ENUM_TO_STRING(Type, ...)														
+class s3EnumUtil
+{
+public:
+	s3EnumUtil()
+	{
+		std::stringstream si("E_MEMBER1, E_MEMBER2, E_MEMBER3");
+		std::string token;
+		while ((token = getName(si)).length())
+		{
+			stringTable.push_back("MyEnum::" + token);
+			valueTable["MyEnum::" + token] = valueTable[token] = stringTable.size() - 1;
+		}
+	}
+	std::string toString(MyEnum type) const
+	{
+		return stringTable[static_cast<int>(type)];
+	}
+	int toIndex(MyEnum type) const
+	{
+		std::string result = stringTable[static_cast<int>(type)];
+	}
+	MyEnum fromString(std::string s) const
+	{
+		return static_cast<MyEnum>(valueTable.at(s));
+	}
+	MyEnum fromIndex(int index) const
+	{
+		std::string result = stringTable[index];
+		return static_cast<MyEnum>(valueTable.at(result));
+	}
+	static s3EnumUtil& getHelper()
+	{
+		static s3EnumUtil helper;
+		return helper;
+	}
+	static std::string getName(std::stringstream& si)
+	{
+		std::string ret;
+		while (si &&
+			   !((si.peek() >= 'a' && si.peek() <= 'z') ||
+			   (si.peek() >= 'A' && si.peek() <= 'Z') ||
+			   (si.peek() >= '0' && si.peek() <= '9')))
+			si.get();
+		if (!si) return "";
+		while ((si.peek() >= 'a' && si.peek() <= 'z') ||
+			   (si.peek() >= 'A' && si.peek() <= 'Z') ||
+			   (si.peek() >= '0' && si.peek() <= '9'))
+			ret += si.get();
+		return ret;
+	}
+	std::vector<std::string> stringTable;
+	std::map<std::string, int> valueTable;
+};
 
-#define __ENUM_DEFINE(TypeName, ...)                             \
-    enum class TypeName { __VA_ARGS__ };                         \
+//#define __S3ENUM_DEFINE(TypeName, ...)                             \
+//    enum class TypeName { __VA_ARGS__ };                           \
+//
+//#define s3Enum(TypeName, ...)                                      \
+//    __S3ENUM_DEFINE(TypeName, __VA_ARGS__)                         \
+//    __S3ENUM_TO_STRING(TypeName, __VA_ARGS__)
+//
+//#define s3EnumUtil(Type) (s3EnumUtil##Type::getHelper())
 
-#define ENUM_CLASS(TypeName, ...)                                \
-    __ENUM_DEFINE(TypeName, __VA_ARGS__)                         \
-    __ENUM_TO_STRING(TypeName, __VA_ARGS__)
-
-#ifdef REFLECT_NS
-	#define ENUM_REFLECT(TypeName) (::REFLECT_NS::EnumReflectionHelper<TypeName>::getHelper())
-#else
-	#define ENUM_REFLECT(Type) (EnumReflectionHelper##Type::getHelper())
-#endif
-
-ENUM_CLASS(MyEnum,
-		   E_MEMBER1,
-		   E_MEMBER2,
-		   E_MEMBER3);
+//s3Enum(MyEnum,
+//       E_MEMBER1,
+//       E_MEMBER2,
+//       E_MEMBER3);
 
 int main()
 {
-	std::cout << ENUM_REFLECT(MyEnum)->toString(MyEnum::E_MEMBER1) << std::endl;
-	std::cout << (int)ENUM_REFLECT(MyEnum)->fromString("MyEnum::E_MEMBER1") << std::endl;
+	auto xxx = s3EnumUtil::getHelper();
+
+	std::cout << s3EnumUtil::getHelper().toString(MyEnum::E_MEMBER1) << std::endl;
+	std::cout << (int)s3EnumUtil::getHelper().fromString("MyEnum::E_MEMBER1") << std::endl;
 
 	return 0;
 }
