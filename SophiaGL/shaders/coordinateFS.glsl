739dc649d3c11c0a9cb4f0d973b10a41f728ad16
Bentleyjobs <bentleyjobs@gmail.com>
2020-07-28 00:00:04 +0800

    Fixed impoter bug

--- SophiaGL/shaders/coordinateFS.glsl
+++ SophiaGL/shaders/coordinateFS.glsl
@@ -9,7 +9,7 @@ uniform sampler2D texture1;
 
 void main()
 {
-    fragColor = vec4(normal, 1.0f);
+    fragColor = vec4((normal + vec3(1, 1, 1)) / 2.0f, 1.0f);
     //fragColor = vec4(texCoord, 0.0f, 1.0f);
     //fragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);
 
