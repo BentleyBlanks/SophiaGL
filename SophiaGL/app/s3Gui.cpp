a249b5b5a8d7eb7d6383fc605767f178fac2ffbb
Bentleyjobs <bentleyjobs@gmail.com>
2020-08-05 02:19:09 +0800

    Fixed imgui wrong viewport bug

--- SophiaGL/app/s3Gui.cpp
+++ SophiaGL/app/s3Gui.cpp
@@ -921,5 +921,13 @@ void s3ImGuiBeginRender()
 
 void s3ImGuiEndRender()
 {
+    // Rendering
+    ImGui::Render();
+
+    int display_w, display_h;
+    glfwGetFramebufferSize(g_Window, &display_w, &display_h);
+    glViewport(0, 0, display_w, display_h);
+    //glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+    //glClear(GL_COLOR_BUFFER_BIT);
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
