c72133dc8b0d6ced09d18c410fcaeb6e7ba82f18
BentleyJobs <bentleyjobs@gmail.com>
2020-11-04 22:59:21 +0800

    Added some vertex test data files

--- SophiaGL/app/s3Window.cpp
+++ SophiaGL/app/s3Window.cpp
@@ -41,8 +41,8 @@ bool s3Window::init(const char* title, int x, int y, int width, int height)
     if (!glfwInit()) return false;
 
     // Decide GL versions
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     window = glfwCreateWindow(width, height, title, NULL, NULL);
